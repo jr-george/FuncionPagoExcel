010aa7e4dc6b6932f19de6f9cf1293bf4b492103
bet0dev <53548235+jr-george@users.noreply.github.com>
2025-05-14 00:00:36 -0600

    Add files via upload

---
+++ FuncionPagoExcel/Finanzas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FuncionPagoExcel
+{
+	public class Finanzas
+	{
+		public static double Pago(double tasa, int nper, double va, double vf)
+        {
+            double valorActual = (va * tasa) / (1 - Math.Pow((1 + tasa), -nper));
+            double valorFuturo = (vf * tasa) / (Math.Pow((1 + tasa), nper) - 1);
+            return valorActual + valorFuturo;
+        }
+    }
+}
+
---
+++ FuncionPagoExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FuncionPagoExcel;
+
+Console.WriteLine("Funcion pago de Excel implementada en C#");
+double pago=0;
+double tasa = 10; //Tasa del 10%
+int plazo = 12; //Plazo en meses
+double valorActual = 100000; //Valor actual del bien
+double valorFuturo = 0; //Valor futuro o esperado
+
+pago = Finanzas.Pago(tasa / 100, 12, valorActual, valorFuturo);
+Console.WriteLine("Pago: " + pago);
